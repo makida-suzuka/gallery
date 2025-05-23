1df767333d62fd81be589d3982d910b75b982d56
makida-suzuka <s-makida@jinso.co.jp>
2025-05-23 17:24:14 +0900

    [update]ハンバーガーメニューの見た目、headerの実装

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 window.addEventListener('load', function(){
+    // ハンバーガーメニュー 
+    const ham = document.querySelector('#hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
+    const nav = document.querySelector('#navi'); //js-navの要素を取得し、変数navに格納
+    
+    ham.addEventListener('click', function () { //ハンバーガーメニューをクリックしたら
+        ham.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外し
+        nav.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外し
+        console.log(ham , nav);
+    });
 
     
 });
