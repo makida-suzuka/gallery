311ce3bbd26539093818232a03148a2781dccdc8
makida-suzuka <s-makida@jinso.co.jp>
2025-06-05 15:15:18 +0900

    [update]スムーススクロールの追加

--- main.js
+++ main.js
@@ -97,6 +97,19 @@ window.addEventListener('load', function(){
     });
 
     //スムーススクロール
-    
+    const links = document.querySelectorAll('a[href^="#"]')
+    links.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
 
 });
